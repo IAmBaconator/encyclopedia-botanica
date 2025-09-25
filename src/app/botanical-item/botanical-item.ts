7058e0e93dc960e287b704b9d51a8b68253871e5
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-09-25 22:17:01 +0000

    Add dynamic img & title sync to dummy data.

--- src/app/botanical-item/botanical-item.ts
+++ src/app/botanical-item/botanical-item.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_ITEMS } from '../dummy-items';
+
+// Setup randomization to dynamically select a random user from the DUMMY_ITEMS array.
+const randomIndex = Math.floor(Math.random() * DUMMY_ITEMS.length)
+
 @Component({
   selector: 'app-botanical-item',
   templateUrl: './botanical-item.html',
   styleUrl: './botanical-item.css'
 })
 export class BotanicalItem {
+  selectedBotanicalItem = DUMMY_ITEMS[randomIndex];
 
+  // Caculate the image path to keep this off the HTML template.
+  get imagePath() {
+    return 'assets/botanical-items' + this.selectedBotanicalItem.avatar
+  }
 }
