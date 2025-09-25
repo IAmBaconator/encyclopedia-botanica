46fd028ca3d1396f5b73a78790e944bfc31875c4
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-09-25 21:26:14 +0000

    Adding temp data to use for populating items

---
+++ src/app/dummy-items.ts
@@ -0,0 +1,32 @@
+export const DUMMY_ITEMS = [
+  {
+    id: 'i1',
+    name: 'flower1',
+    avatar: 'item-1.jpg',
+  },
+  {
+    id: 'i2',
+    name: 'flower2',
+    avatar: 'item-2.jpg',
+  },
+  {
+    id: 'i3',
+    name: 'flower3',
+    avatar: 'item-3.jpg',
+  },
+  {
+    id: 'i4',
+    name: 'flower4',
+    avatar: 'item-4.jpg',
+  },
+  {
+    id: 'i5',
+    name: 'flower5',
+    avatar: 'item-5.jpg',
+  },
+  {
+    id: 'i6',
+    name: 'flower6',
+    avatar: 'item-6.jpg',
+  },
+];
