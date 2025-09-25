7f551ea4288c794b91115321c76cf5c94b92d549
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-09-25 18:35:52 +0000

    Created Initial Template & Static Navigational

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./header/header";
+import { BotanicalItem } from "./botanical-item/botanical-item";
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, BotanicalItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/botanical-item/botanical-item.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botanical-item',
+  templateUrl: './botanical-item.html',
+  styleUrl: './botanical-item.css'
+})
+export class BotanicalItem {
+
+}
