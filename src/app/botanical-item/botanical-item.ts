a05cc80fae02095cfb301c3b5dff576694d732c1
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-09-30 21:53:11 +0000

    Swapped single string sets to combine into one object set.

--- src/app/botanical-item/botanical-item.ts
+++ src/app/botanical-item/botanical-item.ts
@@ -1,5 +1,12 @@
 import { Component, computed, input, output } from '@angular/core';
 
+interface BotanicalItemObj {
+  id: string;
+  avatar: string;
+  name: string;
+  profile: string;
+}
+
 @Component({
   selector: 'app-botanical-item',
   templateUrl: './botanical-item.html',
@@ -8,16 +15,18 @@ import { Component, computed, input, output } from '@angular/core';
 export class BotanicalItem {
 
   // input = special function for newer setup using with signals.
-  id = input.required<string>();
-  avatar = input.required<string>(); // <string> = input<T>(): InputSignal<T>; a TS initialization for an Angular value.
-  name = input.required<string>();
+  //id = input.required<string>();
+  //avatar = input.required<string>(); // <string> = input<T>(): InputSignal<T>; a TS initialization for an Angular value.
+  //name = input.required<string>();
+
+  botanicalItemObj = input.required<BotanicalItemObj>(); // 
   select = output<string>(); // Declares an output that emits strings.
 
   imagePath = computed(() => {
-    return 'assets/botanical-items/' + this.avatar(); // Add back in after I import my own flowers.
+    return 'assets/botanical-items/' + this.botanicalItemObj().avatar; // Add back in after I import my own flowers.
   });
 
   onSelectBotanicalItem() {
-    this.select.emit(this.id());
+    this.select.emit(this.botanicalItemObj().id);
   }
 }
