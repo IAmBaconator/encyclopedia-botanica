790926afc7ebe3be07d9c07c019a4e4d93df78a1
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-10-01 22:27:50 +0000

    Added two-way binding for title, summary, and date.

--- src/app/botanica-details/add-botanica-note/add-botanica-note.ts
+++ src/app/botanica-details/add-botanica-note/add-botanica-note.ts
@@ -1,13 +1,19 @@
 import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-botanica-note',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-botanica-note.html',
   styleUrl: './add-botanica-note.css'
 })
 export class AddBotanicaNote {
   cancel = output<void>();
+
+  //form variables
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
   
   onCancel() {
     this.cancel.emit();
